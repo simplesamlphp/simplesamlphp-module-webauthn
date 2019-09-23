b2870de972d45fac70debb23ef44f423fcd213da
Pavel Břoušek <433364@mail.muni.cz>
2019-09-23 16:30:09 +0200

    move inline Javasript to external
    and separate data from code

--- templates/webauthn.php
+++ templates/webauthn.php
@@ -2,16 +2,14 @@
 
 <?php ob_start(); ?>
     <link rel="stylesheet" type="text/css" href="<?php echo htmlspecialchars(SimpleSAML\Module::getModuleUrl('webauthn/')); ?>assets/css/webauthn.css" />
-    <script>
-        <?php echo $this->data['JSCode']; ?>
-    </script>
+    <meta name="frontendData" id="frontendData" content="<?php echo htmlspecialchars($this->data['frontendData']); ?>" />
  <?php $html = ob_get_contents(); ob_end_clean(); $this->data['head'] = $html; ?>
 <?php $this->includeAtTemplateBase('includes/header.php'); ?>
 
 
     <h1><?php echo htmlspecialchars($this->t('{webauthn:webauthn:heading1}')); ?></h1>
     <h2><?php echo htmlspecialchars($this->t('{webauthn:webauthn:accountEnabled}')); ?></h2>
-    <?php if(((is_array($this->data['FIDO2Tokens']) || $this->data['FIDO2Tokens'] instanceof Countable)?count($this->data['FIDO2Tokens']):strlen($this->data['FIDO2Tokens'])) > 0): ?>
+    <?php if((isset($this->data['FIDO2Tokens'])?((is_array($this->data['FIDO2Tokens']) || $this->data['FIDO2Tokens'] instanceof Countable)?count($this->data['FIDO2Tokens']):strlen($this->data['FIDO2Tokens'])):0) > 0): ?>
         <div id="currentTokens"><span id='tokencaption'><?php echo htmlspecialchars($this->t('{webauthn:webauthn:tokenList}')); ?></span>
             <ul>
                 <?php foreach($this->data['FIDO2Tokens'] as $index => $this->data['token']): ?>
@@ -24,24 +22,23 @@
             </ul>
         </div>
     <?php endif; ?>
-    <?php if(((is_array($this->data['regForm']) || $this->data['regForm'] instanceof Countable)?count($this->data['regForm']):strlen($this->data['regForm'])) > 0): ?>
+    <?php if((isset($this->data['regURL'])?((is_array($this->data['regURL']) || $this->data['regURL'] instanceof Countable)?count($this->data['regURL']):strlen($this->data['regURL'])):0) > 0): ?>
         <form id='regform' method='POST' action='<?php echo $this->data['regURL']; ?>'>
             <input type='hidden' id='resp' name='response_id' value='0'/>
             <input type='hidden' id='data' name='attestation_client_data_json' value='nix'/>
             <input type='hidden' id='attobj' name='attestation_object' value='mehrnix'/>
             <input type='hidden' id='type' name='type' value='something'/>
             <input type='hidden' id='operation' name='operation' value='REG'/>
-            <button type='button' onClick="<?php echo $this->data['regForm']; ?>" onsubmit='false' ><?php echo htmlspecialchars($this->t('{webauthn:webauthn:newTokenButton}')); ?></button>
+            <button type='button' id='regformSubmit'><?php echo htmlspecialchars($this->t('{webauthn:webauthn:newTokenButton}')); ?></button>
             <?php echo htmlspecialchars($this->t('{webauthn:webauthn:newTokenName}')); ?> <input 
                 type='text' 
                 id='tokenname' 
                 name='tokenname' 
                 size='40' 
-                value='<?php echo htmlspecialchars($this->t('{webauthn:webauthn:newTokenDefaultName}')); ?> <?php echo htmlspecialchars((new DateTime("now", new DateTimeZone("Europe/Luxembourg")))->format('Y-m-d')); ?>' onkeydown="if (event.keyCode == 13)
-                            return false;"/>
+                value='<?php echo htmlspecialchars($this->t('{webauthn:webauthn:newTokenDefaultName}')); ?> <?php echo htmlspecialchars((new DateTime("now", new DateTimeZone("Europe/Luxembourg")))->format('Y-m-d')); ?>' />
         </form>
         <div class='space'></div>
-        <?php if(((is_array($this->data['FIDO2Tokens']) || $this->data['FIDO2Tokens'] instanceof Countable)?count($this->data['FIDO2Tokens']):strlen($this->data['FIDO2Tokens'])) > 0): ?>
+        <?php if((isset($this->data['FIDO2Tokens'])?((is_array($this->data['FIDO2Tokens']) || $this->data['FIDO2Tokens'] instanceof Countable)?count($this->data['FIDO2Tokens']):strlen($this->data['FIDO2Tokens'])):0) > 0): ?>
             <?php foreach($this->data['FIDO2Tokens'] as $index => $this->data['token']): ?>
                 <?php if($this->data['FIDO2AuthSuccessful'] != $this->data['token'][0]): ?>
                     <form class='deleteform' id='delete-<?php echo htmlspecialchars($index); ?>' method='POST' action='<?php echo $this->data['delURL']; ?>'>
@@ -56,7 +53,7 @@
             </form>
         <?php endif; ?>
     <?php endif; ?>
-    <?php if(((is_array($this->data['authForm']) || $this->data['authForm'] instanceof Countable)?count($this->data['authForm']):strlen($this->data['authForm'])) > 0): ?>
+    <?php if((isset($this->data['authURL'])?((is_array($this->data['authURL']) || $this->data['authURL'] instanceof Countable)?count($this->data['authURL']):strlen($this->data['authURL'])):0) > 0): ?>
         <form id='authform' method='POST' action='<?php echo $this->data['authURL']; ?>'>
             <input type='hidden' id='resp' name='response_id' value='0'/>
             <input type='hidden' id='data_raw_b64' name='client_data_raw' value='garnix'/>
@@ -66,13 +63,14 @@
             <!-- ignoring <input type='hidden' id='userhandle' name='userhandle' value='someuser'/> -->
             <input type='hidden' id='type' name='type' value='something'/>
             <input type='hidden' id='operation' name='operation' value='AUTH'/>
-            <input type='checkbox' id='credentialChange' name='credentialChange'><?php if(((is_array($this->data['FIDO2Tokens']) || $this->data['FIDO2Tokens'] instanceof Countable)?count($this->data['FIDO2Tokens']):strlen($this->data['FIDO2Tokens'])) < 2): ?>
+            <input type='checkbox' id='credentialChange' name='credentialChange'><?php if((isset($this->data['FIDO2Tokens'])?((is_array($this->data['FIDO2Tokens']) || $this->data['FIDO2Tokens'] instanceof Countable)?count($this->data['FIDO2Tokens']):strlen($this->data['FIDO2Tokens'])):0) < 2): ?>
             <?php echo htmlspecialchars($this->t('{webauthn:webauthn:wantsAdd}')); ?>
         <?php else: ?>
             <?php echo htmlspecialchars($this->t('{webauthn:webauthn:wantsModification}')); ?>
         <?php endif; ?></input><br/>
-        <button type='button' onClick="<?php echo $this->data['authForm']; ?>" onsubmit='false' ><?php echo htmlspecialchars($this->t('{webauthn:webauthn:authTokenButton}')); ?></button>
+        <button type='button' id='authformSubmit'><?php echo htmlspecialchars($this->t('{webauthn:webauthn:authTokenButton}')); ?></button>
     </form>
     <?php endif; ?>
+    <script src="<?php echo htmlspecialchars(SimpleSAML\Module::getModuleUrl('webauthn/')); ?>assets/js/webauthn.js"></script>
         
 <?php $this->includeAtTemplateBase('includes/footer.php'); ?>
--- www/webauthn.php
+++ www/webauthn.php
@@ -34,131 +34,49 @@ $translator = $t->getTranslator();
 $t->data['UserID'] = $state['FIDO2Username'];
 $t->data['FIDO2Tokens'] = $state['FIDO2Tokens'];
 
-$t->data['JSCode'] = "
-// the following two functions are taken from https://stackoverflow.com/questions/16363419/how-to-get-binary-string-from-arraybuffer
-
-function BinaryToString(binary)
-{
-    var error;
-
-    try {
-        return decodeURIComponent(escape(binary));
-    } catch (_error) {
-        error = _error;
-        if (error instanceof URIError) {
-            return binary;
-        } else {
-            throw error;
-        }
-    }
-}
-
-function ArrayBufferToString(buffer)
-{
-    return BinaryToString(String.fromCharCode.apply(null, Array.prototype.slice.apply(new Uint8Array(buffer))));
-}
-";
-
 $challenge = str_split($state['FIDO2SignupChallenge'], 2);
 $username = str_split(hash('sha512', $state['FIDO2Username'] . '|' . Utils\Config::getSecretSalt() . '|' . $state['Source']['entityid']), 2);
 
-$challengeEncoded = "";
+$challengeEncoded = [];
 foreach ($challenge as $oneChar) {
-    $challengeEncoded .= "0x$oneChar, ";
+    $challengeEncoded[] = hexdec($oneChar);
 }
 
 foreach ($state['FIDO2Tokens'] as $number => $token) {
     $idSplit = str_split($token[0], 2);
-    $credentialIdEncoded[$number] = "";
+    $credentialIdEncoded[$number] = [];
     foreach ($idSplit as $credIdBlock) {
-        $credentialIdEncoded[$number] .= "0x$credIdBlock, ";
+        $credentialIdEncoded[$number][] = hexdec($credIdBlock);
     }
 }
 
-$usernameEncoded = "";
+$usernameEncoded = [];
 foreach ($username as $oneChar) {
-    $usernameEncoded .= "0x$oneChar, ";
+    $usernameEncoded[] = hexdec($oneChar);
 }
 
+$frontendData = [];
+$frontendData['challengeEncoded'] = $challengeEncoded;
+$frontendData['state'] = [];
+foreach (['Source','FIDO2Scope','FIDO2Username','FIDO2Displayname','requestTokenModel'] as $stateItem) {
+    $frontendData['state'][$stateItem] = $state[$stateItem];
+}
+$frontendData['usernameEncoded'] = $usernameEncoded;
+$frontendData['attestation'] = $state['requestTokenModel'] ? "indirect" : "none";
+$frontendData['credentialIdEncoded'] = $credentialIdEncoded;
+$t->data['frontendData'] = json_encode($frontendData);
+
 $t->data['FIDO2AuthSuccessful'] = $state['FIDO2AuthSuccessful'];
-$t->data['regForm'] = "";
 if (count($state['FIDO2Tokens']) == 0 || ($state['FIDO2WantsRegister'] === true && $state['FIDO2AuthSuccessful'] !== false)) {
     $t->data['regURL'] = Module::getModuleURL('webauthn/regprocess.php?StateId=' . urlencode($id));
     $t->data['delURL'] = Module::getModuleURL('webauthn/managetoken.php?StateId=' . urlencode($id));
-    $t->data['regForm'] = "navigator.credentials.create(publicKeyCredentialCreationOptions)
-    .then((cred) => {
-        console.log('NEW CREDENTIAL', cred);
-        document.getElementById('resp').value = cred.id;
-        var enc = new TextDecoder('utf-8');
-        document.getElementById('data').value = enc.decode(cred.response.clientDataJSON);
-        document.getElementById('attobj').value = btoa(ArrayBufferToString(cred.response.attestationObject));
-        document.getElementById('type').value = cred.response.type;
-        document.forms['regform'].submit();
-    })
-    .then((assertion) => {
-        console.log('ASSERTION', assertion);
-    })
-    .catch((err) => {
-        console.log('ERROR', err);
-    });";
-    $t->data['JSCode'] .= "
-var publicKeyCredentialCreationOptions = {
-    publicKey: {
-      challenge: new Uint8Array([ " . $challengeEncoded . " ]).buffer, 
-      rp: {
-          name: '" . $state['Source']['entityid'] . "',
-          ".is_null($state['FIDO2Scope']) ? '' : ("id: '".$state['FIDO2Scope']."'"). ",
-      },
-      user: {
-	  id: new Uint8Array([ " . $usernameEncoded . " ]).buffer,
-          name: '" . $state['FIDO2Username'] . "',
-          displayName: '" . $state['FIDO2Displayname'] . "',
-      },
-      pubKeyCredParams: [{alg: -7, type: 'public-key'}],
-      timeout: 60000,
-      attestation: '".($state['requestTokenModel'] ? "indirect" : "none") . "',
-  }
-};";
 }
 
 $t->data['authForm'] = "";
 if (count($state['FIDO2Tokens']) > 0 && ($state['FIDO2WantsRegister'] !== true || $state['FIDO2AuthSuccessful'] === false)) {
     $t->data['authURL'] = Module::getModuleURL('webauthn/authprocess.php?StateId=' . urlencode($id));
-    $t->data['authForm'] = "navigator.credentials.get(publicKeyCredentialRequestOptions)
-    .then((cred) => {
-        console.log('NEW CREDENTIAL', cred);
-        document.getElementById('resp').value = cred.id;
-        var enc = new TextDecoder('utf-8');
-        document.getElementById('data_raw_b64').value = btoa(ArrayBufferToString(cred.response.clientDataJSON));
-        document.getElementById('data').value = enc.decode(cred.response.clientDataJSON);
-        document.getElementById('authdata').value = btoa(ArrayBufferToString(cred.response.authenticatorData));
-	document.getElementById('sigdata').value = btoa(ArrayBufferToString(cred.response.signature));
-        document.getElementById('type').value = cred.response.type;
-        document.forms['authform'].submit();
-    })
-    .then((assertion) => {
-        console.log('ASSERTION', assertion);
-    })
-    .catch((err) => {
-        console.log('ERROR', err);
-    });";
-    $t->data['JSCode'] .= "const publicKeyCredentialRequestOptions = {
-    publicKey: {
-        challenge: new Uint8Array([ " . $challengeEncoded . " ]).buffer,
-	rpId: '" . $state['FIDO2Scope'] . "',
-        allowCredentials: [\n";
-    foreach ($credentialIdEncoded as $oneId) {
-        $t->data['JSCode'] .= "{id: new Uint8Array([ " . $oneId . "  ]).buffer , type: 'public-key' },\n";
-    }
-    $t->data['JSCode'] .= "
-        ],
-        timeout: 60000,
-    }
-}
-";
 }
 
 // dynamically generate the JS code needed for token registration
 
 $t->show();
-
