32cc2b5278686a07fab87b3a2fd09bd83ae44909
Pavel Břoušek <433364@mail.muni.cz>
2019-09-24 14:52:08 +0200

    fix indent in template(s)

--- templates/webauthn.php
+++ templates/webauthn.php
@@ -10,15 +10,16 @@
     <h1><?php echo htmlspecialchars($this->t('{webauthn:webauthn:heading1}')); ?></h1>
     <h2><?php echo htmlspecialchars($this->t('{webauthn:webauthn:accountEnabled}')); ?></h2>
     <?php if((isset($this->data['FIDO2Tokens'])?((is_array($this->data['FIDO2Tokens']) || $this->data['FIDO2Tokens'] instanceof Countable)?count($this->data['FIDO2Tokens']):strlen($this->data['FIDO2Tokens'])):0) > 0): ?>
-        <div id="currentTokens"><span id='tokencaption'><?php echo htmlspecialchars($this->t('{webauthn:webauthn:tokenList}')); ?></span>
+        <div id="currentTokens">
+            <span id='tokencaption'><?php echo htmlspecialchars($this->t('{webauthn:webauthn:tokenList}')); ?></span>
             <ul>
                 <?php foreach($this->data['FIDO2Tokens'] as $index => $this->data['token']): ?>
                     <?php if($this->data['FIDO2AuthSuccessful'] == false or $this->data['FIDO2AuthSuccessful'] != $this->data['token'][0]): ?>
                         <li class='othertoken'><?php echo htmlspecialchars($this->data['token'][3]); ?></li>
-                        <?php else: ?>
+                    <?php else: ?>
                         <li class='currenttoken'><?php echo htmlspecialchars($this->data['token'][3]); ?> <?php echo htmlspecialchars($this->t('{webauthn:webauthn:currentToken}')); ?></li>
-                        <?php endif; ?>
-                    <?php endforeach;?>
+                    <?php endif; ?>
+                <?php endforeach;?>
             </ul>
         </div>
     <?php endif; ?>
@@ -30,11 +31,8 @@
             <input type='hidden' id='type' name='type' value='something'/>
             <input type='hidden' id='operation' name='operation' value='REG'/>
             <button type='button' id='regformSubmit'><?php echo htmlspecialchars($this->t('{webauthn:webauthn:newTokenButton}')); ?></button>
-            <?php echo htmlspecialchars($this->t('{webauthn:webauthn:newTokenName}')); ?> <input 
-                type='text' 
-                id='tokenname' 
-                name='tokenname' 
-                size='40' 
+            <?php echo htmlspecialchars($this->t('{webauthn:webauthn:newTokenName}')); ?>
+            <input type='text' id='tokenname' name='tokenname' size='40'
                 value='<?php echo htmlspecialchars($this->t('{webauthn:webauthn:newTokenDefaultName}')); ?> <?php echo htmlspecialchars((new DateTime("now", new DateTimeZone("Europe/Luxembourg")))->format('Y-m-d')); ?>' />
         </form>
         <div class='space'></div>
@@ -43,13 +41,17 @@
                 <?php if($this->data['FIDO2AuthSuccessful'] != $this->data['token'][0]): ?>
                     <form class='deleteform' id='delete-<?php echo htmlspecialchars($index); ?>' method='POST' action='<?php echo $this->data['delURL']; ?>'>
                         <input type='hidden' id='credId-<?php echo htmlspecialchars($index); ?>' name='credId' value='<?php echo htmlspecialchars($this->data['token'][0]); ?>'/>
-                        <button type='submit' id='submit-<?php echo htmlspecialchars($index); ?>' name='submit' value='DELETE'><?php echo htmlspecialchars($this->t('{webauthn:webauthn:removePrefix}')); ?> &quot;<?php echo htmlspecialchars($this->data['token'][3]); ?>&quot;</button>
+                        <button type='submit' id='submit-<?php echo htmlspecialchars($index); ?>' name='submit' value='DELETE'>
+                            <?php echo htmlspecialchars($this->t('{webauthn:webauthn:removePrefix}')); ?> &quot;<?php echo htmlspecialchars($this->data['token'][3]); ?>&quot;
+                        </button>
                     </form>
                 <?php endif; ?>
             <?php endforeach;?>
             <div class='space'></div>
             <form id='nevermind' method='POST' action='<?php echo $this->data['delURL']; ?>'>
-                <button type='submit' id='submit-nevermind' name='submit' value='NEVERMIND'><?php echo htmlspecialchars($this->t('{webauthn:webauthn:noChange}')); ?></button>
+                <button type='submit' id='submit-nevermind' name='submit' value='NEVERMIND'>
+                    <?php echo htmlspecialchars($this->t('{webauthn:webauthn:noChange}')); ?>
+                </button>
             </form>
         <?php endif; ?>
     <?php endif; ?>
@@ -71,9 +73,9 @@
                     <?php echo htmlspecialchars($this->t('{webauthn:webauthn:wantsModification}')); ?>
                 <?php endif; ?>
             </label><br/>
-        <button type='button' id='authformSubmit'><?php echo htmlspecialchars($this->t('{webauthn:webauthn:authTokenButton}')); ?></button>
-    </form>
+            <button type='button' id='authformSubmit'><?php echo htmlspecialchars($this->t('{webauthn:webauthn:authTokenButton}')); ?></button>
+        </form>
     <?php endif; ?>
     <script src="<?php echo htmlspecialchars(SimpleSAML\Module::getModuleUrl('webauthn/')); ?>assets/js/webauthn.js"></script>
-        
+
 <?php $this->includeAtTemplateBase('includes/footer.php'); ?>
