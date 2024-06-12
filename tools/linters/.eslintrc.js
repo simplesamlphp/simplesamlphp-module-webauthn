8c22cb8e22298e3d4d42f9c8822ec6badef37e3b
Tim van Dijen <tvdijen@gmail.com>
2024-06-12 23:37:40 +0200

    Update eslint config

---
+++ tools/linters/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ["json:recommended"],
+    ignorePatterns: ["!/config/webauthn-aaguid.json"],
+    parserOptions: {
+        ecmaVersion: 2015,
+        sourceType: "module"
+    }
+};
